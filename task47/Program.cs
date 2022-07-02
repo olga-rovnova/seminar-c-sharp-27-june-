f5c4af5b16c850db6265a980c8ea8bd178127132
Olga <olga-rovnova@yandex.ru>
2022-07-02 20:17:41 +0300

    task 47 file

---
+++ task47/Program.cs
@@ -0,0 +1,35 @@
+﻿//Задача 47: Задайте двумерный массив размером m×n, 
+//заполненный случайными вещественными числами.
+
+int m = Input("введите m: ");
+int n = Input("введите n: ");
+
+double[,] numbers = new double[m, n];
+
+for (int i = 0; i < numbers.GetLength(0); i++)
+{
+    for (int j = 0; j < numbers.GetLength(1); j++)
+    {
+        numbers[i, j] = Math.Round((new Random().Next(100, 1000) * 0.01 + new Random().Next(10)), 2);
+    }
+}
+
+PrintArray(numbers);
+
+int Input(string output)
+{
+    Console.Write(output);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+void PrintArray(double[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write(array[i, j] + " ");
+        }
+        Console.WriteLine();
+    }
+}
